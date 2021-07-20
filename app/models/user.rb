b8148d5097067d97ee393a4cdf6d27d913273bde
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-07-20 22:23:01 +0900

    usersモデルのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  NAME_REGIX = /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true, format: { with: NAME_REGIX }
+  validates :last_name, presence: true, format: { with: NAME_REGIX }
+  NAME_KANA_REGIX = /\A[ァ-ヶー－]+\z/
+  validates :first_name_kana, presence: true, format: { with: NAME_KANA_REGIX }
+  validates :last_name_kana, presence: true, format: { with: NAME_KANA_REGIX }
   validates :birthday, presence: true
 end
