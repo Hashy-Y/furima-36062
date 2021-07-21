338e9092430474a16a0e9668735711c30c5a2ed8
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-07-22 00:29:22 +0900

    出品商品のactive_hash設定とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :price
+    with_options numericality: { other_than: 1 , message: "can't be blank" } do
+      validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+      validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" } 
+      validates :postage_id, numericality: { other_than: 1 , message: "can't be blank" }
+      validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank" }
+      validates :delivery_period_id, numericality: { other_than: 1 , message: "can't be blank" }
+    end
+  end
 end
