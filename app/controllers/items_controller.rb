5c5d97fd0e6a5422a6d682d7d5243d5428f79206
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-07-26 20:44:44 +0900

    商品削除機能のルーティング及びコントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_prototype, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -32,6 +32,10 @@ class ItemsController < ApplicationController
     else
       render action: :edit
     end 
+  end
+
+  def destroy
+    
   end
 
   private
